87ea60b117be2195138126c62360fbe326ceb636
m-kann0 <m.kanno.0915@gmail.com>
2017-07-07 22:14:05 +0900

    First commit

---
+++ src/main/java/com/ddd/example/DddExampleApplication.java
@@ -0,0 +1,12 @@
+package com.ddd.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddExampleApplication.class, args);
+	}
+}
